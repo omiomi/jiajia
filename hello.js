513710357847caf26ad9b20bb0e4e151096bf392
19534212@qq.com <hxfq5166>
2018-04-04 11:53:40 +0800

    commit

--- hello.js
+++
@@ -1,9 +0,0 @@
-var http = require("http");
-http.createServer(function(request, response) {
-	response.writeHead(200, {
-		"Content-Type" : "text/plain" // 输出类型
-	});
-	response.write("Hello World");// 页面输出
-	response.end();
-}).listen(8000); // 监听端口号
-console.log("nodejs start listen 8000 port!");
