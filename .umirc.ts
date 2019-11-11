173ddf76a2e3df70544beb292808105dae822fad
王遥 <zhxzhs@gmail.com>
2019-11-11 19:21:22 +0800

    a

--- .umirc.ts
+++ .umirc.ts
@@ -1,33 +1,46 @@
-import { IConfig } from 'umi-types';
+import { IConfig } from 'umi-types'; // ref: https://umijs.org/config/
 
-// ref: https://umijs.org/config/
-const config: IConfig =  {
+const config: IConfig = {
   treeShaking: true,
   routes: [
     {
       path: '/',
       component: '../layouts/index',
       routes: [
-        { path: '/', component: '../pages/index' }
-      ]
-    }
+        {
+          name: '个人中心',
+          path: '/accountcenter',
+          component: './accountcenter',
+        },
+        {
+          path: '/',
+          component: '../pages/index',
+        },
+      ],
+    },
   ],
   plugins: [
     // ref: https://umijs.org/plugin/umi-plugin-react.html
-    ['umi-plugin-react', {
-      antd: true,
-      dva: false,
-      dynamicImport: false,
-      title: 'jiajia',
-      dll: false,
-      
-      routes: {
-        exclude: [
-          /components\//,
-        ],
+    [
+      'umi-plugin-react',
+      {
+        antd: true,
+        dva: {
+          immer: true,
+        },
+        dynamicImport: false,
+        title: 'jiajia',
+        dll: false,
+        routes: {
+          exclude: [/components\//],
+        },
+        locale: true,
       },
-    }],
+    ],
   ],
-}
-
+  mock: {
+    exclude: ['abc', 'add'],
+  },
+};
 export default config;
+
